39083aaf611fa0e9b74ca4e04e2e36d4412457f0
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2021-10-02 22:40:37 -0300

    firebase setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Lets build Snapchat</h1>
+      <WebcamCapture />
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -10,3 +10,9 @@ const firebaseConfig = {
   };
 
   const firebase = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { db, auth, storage, provider };
