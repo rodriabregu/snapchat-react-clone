0881c0be13eab50b03f26b1a8d777707bcb78e9b
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2021-10-02 21:20:03 -0300

    init base

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcC092gDrUsSnKqmbYEMiuBZVbpmyMV2A",
+    authDomain: "snapchat-clone-31828.firebaseapp.com",
+    projectId: "snapchat-clone-31828",
+    storageBucket: "snapchat-clone-31828.appspot.com",
+    messagingSenderId: "752787071258",
+    appId: "1:752787071258:web:8c5288f3b6de9fd76fcca3"
+  };
+
+  const firebase = firebase.initializeApp(firebaseConfig);
